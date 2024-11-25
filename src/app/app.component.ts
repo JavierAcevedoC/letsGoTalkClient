76eee0b0c07edabfd082c3f7cd0d80fdba7fd694
Javier Acevedo C <javier.iacevedoc@gmail.com>
2024-11-25 02:26:52 -0300

    feat(style): neon style i think get a great atmospher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,70 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { ChatService } from './service/chat.service';
+import { Subscription } from 'rxjs';
+import { ChatContainerComponent } from './component/chat-container/chat-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ChatContainerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'clientws';
+export class AppComponent implements OnDestroy {
 
   chatService = inject(ChatService);
+  private subs: Subscription;
+  messages: any[] = [];
+  currentMessage: string = '';
+  currentUsername: string = '';
 
   constructor() {
-    this.chatService.getMessages().subscribe(message => {
-      console.log('Received message:', message);
-    });
-    const message = {
-      username: "cthulhu",
-      content: "Hello, server!"
+    const username = sessionStorage.getItem('username');
+    if(username) {
+      this.currentUsername = username;
     }
-    this.chatService.sendMessage(message);
+
+    this.subs = this.chatService.getMessages().subscribe(message => {
+      this.messages = [...this.messages, message];
+    });
+    
   }
+
+  getValue(a: Event) {
+    if(this.currentUsername !== '') {
+      this.currentMessage = (a.target as HTMLInputElement).value;
+    } else {
+      this.currentUsername = (a.target as HTMLInputElement).value;
+      this.ok();
+    }
+  }
+
+  ok() {
+    sessionStorage.setItem('username', this.currentUsername);
+  }
+  
+  sendMessage() {
+    const message = {
+      username: this.currentUsername,
+      content: this.currentMessage,
+      room: "room1"
+    }
+    
+    if(this.currentMessage.length > 0) {
+      this.chatService.sendMessage(message);
+    }
+
+    const input = document.getElementById('typping') as HTMLInputElement;
+    input.value = '';
+    input.focus();
+    this.currentMessage = '';
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+    this.chatService.closeConnection();
+  }
+
 }
