baf0a04a93695e47cb851d927d22ea5430584b19
Javier Acevedo C <javier.iacevedoc@gmail.com>
2024-11-24 21:10:52 -0300

    feat(Chat): Basic way to consume ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatService } from './service/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'clientws';
+
+  chatService = inject(ChatService);
+
+  constructor() {
+    this.chatService.getMessages().subscribe(message => {
+      console.log('Received message:', message);
+    });
+    const message = {
+      username: "cthulhu",
+      content: "Hello, server!"
+    }
+    this.chatService.sendMessage(message);
+  }
 }
